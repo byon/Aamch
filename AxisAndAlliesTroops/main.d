aba596fe61e9aff4a9a53616ef6620e987651941
Marko Raatikainen <marko.m.raatikainen@gmail.com>
2011-07-26 21:35:44 +0300

    -Added a prototype for axis and allies helper

---
+++ AxisAndAlliesTroops/main.d
@@ -0,0 +1,120 @@
+module main;
+
+import std.array;
+import std.conv;
+import std.stdio;
+import std.traits;
+
+struct Troop
+{
+    string name;
+    double cost;
+    uint speed;
+    uint frontDefense;
+    uint rearDefense;
+    Attack soldierAttack;
+    Attack vehicleAttack;
+    string type;
+    string subType;
+    string nation;
+    uint year;
+    string specialAbilities;
+    uint commandValue;
+    string commandEffect;
+    string rarity;
+    string id;
+    string set;
+    
+    string toString( )
+    {
+        return name ~ " " ~
+               to!string(cost) ~ " " ~
+               to!string(speed) ~ " " ~
+               to!string(frontDefense) ~ " " ~
+               to!string(rearDefense) ~ " " ~
+               to!string(soldierAttack) ~ " " ~
+               to!string(vehicleAttack) ~ " " ~
+               type ~ " " ~
+               subType ~ " " ~
+               nation ~ " " ~
+               to!string(year) ~ " " ~
+               specialAbilities ~ " " ~
+               to!string(commandValue) ~ " " ~
+               commandEffect ~ " " ~
+               rarity ~ " " ~
+               id ~ " " ~
+               set;
+    }
+}
+
+struct Attack
+{
+    uint shortDistance;
+    uint mediumDistance;
+    uint longDistance;
+    
+    string toString( )
+    {
+        return to!string(shortDistance) ~ " " ~ to!string(mediumDistance) ~ " " ~
+               to!string(longDistance);
+    }
+}
+
+T To(T)(string source)
+{
+    static if (__traits(isArithmetic, T))
+    {
+        if (source.length == 0)
+        {
+            return T.init;
+        }
+    }
+    
+    return to!T(source);
+}
+
+Troop CreateTroop(string[] tokens)
+{
+    assert (tokens.length > 0);
+    return Troop(tokens[0], To!double(tokens[1]));
+}
+
+void HandleTokens(string[] tokens, ref Troop[] troops)
+{
+    troops ~= CreateTroop(tokens);
+}
+
+void HandleLine(string line, ref Troop[] troops)
+{
+    HandleTokens(split(line, "\t"), troops);
+}
+
+int main(string[] arguments)
+{
+    Troop[] troops;
+    foreach (string line; lines(File(arguments[1])))
+    {
+        /// @todo What if two chars for end of line?
+        HandleLine(line[0..$-1], troops);
+    }
+    
+    foreach (Troop troop; troops)
+    {
+        writeln(troop);
+    }
+
+    auto types = __traits(allMembers, Troop);
+    foreach (t; types)
+    {
+        if (isSomeFunction!(t))
+        {
+            writeln("Skipping method ", t);
+        }
+        writeln(typeid(t), " ", t);
+    }
+    
+    char[] dummy;
+    readln(dummy);
+    
+    return 0;
+}

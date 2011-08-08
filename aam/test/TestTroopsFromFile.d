1cd9a43d31906d13995433fb41e2154679c23fa7
Marko Raatikainen <marko.m.raatikainen@gmail.com>
2011-08-08 14:12:04 +0300

    - Removed redundant tests (tested on higher level as well) - Reorganized tests to be in the same order as the implementation

--- aam/test/TestTroopsFromFile.d
+++ aam/test/TestTroopsFromFile.d
@@ -5,24 +5,17 @@ import std.exception;
 
 unittest
 {
-    Compare([], TroopsFromInput(new string[0]));
-    Compare([Troop("name", 1)], TroopsFromInput([CreateTroopString( )]));
-    Compare([Troop("name", 1), Troop("name", 1)],
-            TroopsFromInput([CreateTroopString( ), CreateTroopString( )]));
-
-    assertThrown!CannotOpenFile(OpenFile("noSuchFile"));
-    {
-        std.file.write("temporary", "abc");
-        scope(exit) std.file.remove("temporary");
-        Compare("abc", OpenFile("temporary").readln( ));
-    }
-
     assertThrown!CannotOpenFile(TroopsFromFile("noSuchFile"));
     {
         std.file.write("temporary", CreateTroopString( ));
         scope(exit) std.file.remove("temporary");
         Compare([Troop("name", 1)], TroopsFromFile("temporary"));
     }
+
+    Compare([], TroopsFromInput(new string[0]));
+    Compare([Troop("name", 1)], TroopsFromInput([CreateTroopString( )]));
+    Compare([Troop("name", 1), Troop("name", 1)],
+            TroopsFromInput([CreateTroopString( ), CreateTroopString( )]));
 }
 
 string CreateTroopString( )

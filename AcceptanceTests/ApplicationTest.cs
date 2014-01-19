1cd21a10c2b97ad85577a09ed939c20be6cb22f8
Marko Raatikainen <marko.m.raatikainen@gmail.com>
2014-01-19 15:44:44 +0200

    Code for launching tested application

--- AcceptanceTests/ApplicationTest.cs
+++ AcceptanceTests/ApplicationTest.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.WindowItems;
 
 namespace AcceptanceTests
 {
     [TestClass]
     public class ApplicationTest
     {
+        private const string APPLICATION_BASE = @"..\..\..\Aamch\bin\";
+#if DEBUG
+        private const string CONFIGURATION = @"Debug\";
+#else
+        private const string CONFIGURATION = "Release";
+#endif
+        private const string APPLICATION_DIRECTORY = APPLICATION_BASE +
+                                                     CONFIGURATION;
+        private const string APPLICATION_NAME = "Aamch.exe";
+        private const string APPLICATION = APPLICATION_DIRECTORY +
+                                           APPLICATION_NAME;
+
         [TestMethod]
-        public void TestMethod1()
+        public void AccessMainWindow()
         {
+            var application = Application.Launch(APPLICATION);
+            var window = application.GetWindow("MainWindow",
+                                               InitializeOption.NoCache);
         }
     }
 }

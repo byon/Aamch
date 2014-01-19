8088944e7cdd1e58ee66120de28551ff5fa18c4f
Marko Raatikainen <marko.m.raatikainen@gmail.com>
2014-01-19 15:19:19 +0200

    Rename test file

--- AcceptanceTests/ApplicationTest.cs
+++ AcceptanceTests/ApplicationTest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace AcceptanceTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ApplicationTest
     {
         [TestMethod]
         public void TestMethod1()

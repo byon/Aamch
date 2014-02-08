98026eafee587db63d0caa0cce931448d3c32456
Marko Raatikainen <marko.m.raatikainen@gmail.com>
2014-02-08 23:32:52 +0200

    Add (pending) scenario for listing troop

---
+++ AcceptanceTests/BasicValues.feature
@@ -0,0 +1,9 @@
+﻿Feature: Basic values
+	As a player
+    I want to see the names of troops listed
+    So I can identify them
+
+Scenario: Viewing one name
+	Given that troops include "Barbed Wire"
+	When troops are viewed
+	Then "Barbed Wire" should be included in list of troops
---
+++ AcceptanceTests/ValueSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests
+{
+    [Binding]
+    public class StepDefinitions
+    {
+        [Given(@"that troops include ""(.*)""")]
+        public void GivenThatTroopsInclude(string name)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"troops are viewed")]
+        public void WhenTroopsAreViewed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"""(.*)"" should be included in list of troops")]
+        public void ThenShouldBeIncludedInListOfTroops(string name)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

27378ba2da40c93b6088eaaa8dd02cef382f0def
Marko Raatikainen <marko.m.raatikainen@gmail.com>
2014-02-16 17:44:38 +0200

    Add troop file cleanup logic

--- TestData/RepositoryTest.cs
+++ TestData/RepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,13 +8,35 @@ namespace TestData
     [TestClass]
     public class RepositoryTest
     {
+        private static string TROOP_DIRECTORY = @"Troops\";
+        private static string TROOP_FILE_PATH = TROOP_DIRECTORY + "troop.json";
         private Repository repository = new Repository();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            CleanupTroopData();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            CleanupTroopData();
+        }
 
         [TestMethod]
         [ExpectedException(typeof(Repository.IoFailure))]
         public void WritingFailureIsNoticed()
         {
             repository.Write(@"unexistingFolder\file.json");
+        }
+
+        private void CleanupTroopData()
+        {
+            if (Directory.Exists(TROOP_DIRECTORY))
+            {
+                Directory.Delete(TROOP_DIRECTORY, true);
+            }
         }
     }
 }

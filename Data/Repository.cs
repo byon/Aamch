b81b5d79466ac55e8d1cc55e311fa8b807b061b6
Marko Raatikainen <marko.m.raatikainen@gmail.com>
2014-02-16 17:37:57 +0200

    Add projects for managing troop data

---
+++ Data/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Data
+{
+    public class Repository
+    {
+        public class Troop
+        {
+        }
+
+        public class IoFailure : Exception
+        {
+            public IoFailure(string reason)
+            {
+            }
+        }
+
+        public void Write(string path)
+        {
+            throw new IoFailure("Could not write troop file");
+        }
+    }
+}
---
+++ TestData/RepositoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestData
+{
+    [TestClass]
+    public class RepositoryTest
+    {
+        private Repository repository = new Repository();
+
+        [TestMethod]
+        [ExpectedException(typeof(Repository.IoFailure))]
+        public void WritingFailureIsNoticed()
+        {
+            repository.Write(@"unexistingFolder\file.json");
+        }
+    }
+}
